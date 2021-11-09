3eb1d008fb51b821cb32651d0d4cec58712a0a4d
Adnan Zaki <adnan_zaki@hotmail.co.id>
2021-11-09 14:54:13 +0700

    finalize mobile menu

--- css/resources/example.php
+++ css/resources/example.php
@@ -5,16 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Zaki</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="animate.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="media.css">
 </head>
-<body>
+<body id="main-web">
     <div class="container">
+        <div class="menu-mask" onclick="hideMenu()"></div>
         <div class="header">
             <div class="logo-container">
                 <img class="web-logo" src="logo-only.png" alt="logo website">
             </div>
             <div class="menu-container">
-                <ul class="navigation">
+                <a href="#" class="mobile-menu" onclick="showMenu()"><span class="material-icons">menu</span></a>
+                <ul class="navigation" id="main-menu">
                     <li class="menu-item">
                         <a href="#" class="menu-link">Home</a>
                     </li>
@@ -28,14 +36,16 @@
                         <a href="#" class="menu-link">Kontak</a>
                     </li>
                 </ul>
+                
             </div>
         </div>
         <div class="content-wrapper">
-            <div class="content"></div>
+            <div class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia accusantium. Facere, nostrum! Ducimus ut ipsam, laudantium ipsum placeat laboriosam omnis cupiditate, rem modi, quasi dolores repellat a sunt dolorum. Quam dolorem mollitia aliquam perspiciatis consequuntur fugiat excepturi voluptates quo esse vero veritatis ad consectetur, odit deleniti tempore? Nam sapiente error quaerat repellat praesentium harum pariatur a aperiam nihil cupiditate consectetur provident in odit quos incidunt aliquam omnis minus blanditiis quod, exercitationem alias ex? Vero odio fuga quis aut ipsam non placeat laudantium vel dolor accusantium eveniet illum amet quas voluptas alias, modi neque dolore, nobis a perspiciatis soluta magni. Modi debitis provident mollitia consectetur odio? Rerum optio fugiat amet aspernatur corporis odit totam, dolor obcaecati, quos ratione labore maiores unde dolorum at cupiditate. Aspernatur recusandae labore fugiat repellendus cupiditate minima quod provident eos voluptatum voluptatibus, ab, tempore ipsam, sed voluptas corporis! Aut fugit recusandae voluptas nihil. Tempora, minima officiis.</div>
             <div class="sidebar"></div>
+            <div class="footer">Copyright 2021 &copy; Adnan Zaki</div>
         </div>
-        <div class="footer"></div>
     </div>
-    <footer></footer>
+
+    <script src="main.js"></script>
 </body>
 </html>

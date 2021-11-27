595840d4951738a366a666e0bddce7a9402714b3
Qais Abdul Aziz <qaizzaa@gmail.com>
2021-11-27 08:12:41 +0700

    update

--- css/blog1.php
+++ css/blog1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog CSS</title>
+    <title>Blog Qais</title>
     <link rel="stylesheet" href="blog1.css">
 </head>
 
@@ -32,34 +32,30 @@
                 </ul>
             </div>
         </div>
-        <div class="content-wrapper cf">
+        <div class="content-wrapper">
             <div class="content">
-                <h1 class="judul">Judul Pertama</h1>
-                <p class="paragraf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores officia dicta quaerat, minus quia architecto hic beatae tempora necessitatibus quidem eum molestiae voluptates fugiat atque, maxime amet libero laborum ea pariatur eaque iusto labore nihil sint fugit. Cumque repellat neque quasi ducimus odio accusamus eligendi sint velit nulla doloribus. Quisquam culpa praesentium dolore itaque cupiditate debitis blanditiis nam illum consectetur temporibus. Quibusdam, tempore quasi aspernatur a facilis rerum quo. Quo molestias laborum, ex temporibus inventore accusamus! Totam similique illo laudantium. Provident a facere vitae ex quam iusto! Repellat praesentium, sed laudantium officia magnam adipisci recusandae illo provident voluptatum, hic, delectus sunt reprehenderit voluptas aut temporibus quas eos sit numquam veritatis pariatur aspernatur? Dolores nesciunt natus quos quae alias, enim culpa consectetur numquam harum debitis expedita molestias doloremque aliquid vero reprehenderit a quidem! Rerum, fuga molestiae quos nobis laudantium sapiente nihil quas ad aliquid, veritatis atque labore quibusdam! Amet porro qui tempore adipisci, molestias provident itaque saepe exercitationem ab facere harum, aut numquam! Animi voluptatem expedita nemo architecto ipsa sunt incidunt natus modi. Ad accusamus, quod, esse sapiente repellendus, at velit minus unde asperiores reprehenderit tenetur consectetur tempora sunt dolorem consequatur atque iste deleniti voluptates incidunt libero distinctio provident? Dolores aperiam perferendis consequuntur, magnam eligendi facere sequi! Officiis sapiente corrupti ullam sequi deleniti amet beatae nobis natus accusantium cum enim provident numquam assumenda sed deserunt quisquam, laborum praesentium eligendi inventore odio, officia voluptas, vel quos? Obcaecati corporis eos possimus unde itaque quam exercitationem! Eveniet similique, accusantium error a eligendi ipsam sed earum consequuntur voluptatem ipsa ad iusto sint laboriosam et aliquid omnis vero? Culpa deleniti, mollitia quia hic accusamus beatae placeat error natus at, molestiae voluptate quis velit nulla dolor omnis cumque eaque quae sapiente eius recusandae eum, minima similique nesciunt. Possimus quae voluptate explicabo nemo repellendus officia delectus totam culpa eaque fugit, distinctio recusandae a, repudiandae dolorum id fuga voluptas consequatur veniam rem impedit tempora voluptatum earum! Ducimus maiores aut, fuga blanditiis esse enim ullam nostrum in, officia eligendi porro officiis consequatur ratione sed culpa. Culpa nulla, cupiditate amet minima veritatis debitis doloribus repellat cumque id veniam nisi quisquam ipsum modi provident dicta! A quisquam labore obcaecati accusantium earum, facilis modi commodi iure est suscipit voluptate deserunt ipsa pariatur! Labore, dicta praesentium porro id assumenda dolore rem quibusdam obcaecati aut explicabo velit cupiditate perspiciatis saepe delectus hic ipsa, vel sapiente reprehenderit non illo quod! Eum consequatur ex non eaque impedit!</p>
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                    <div class="post-card">
+                        <div class="post-header">
+                            <h2 class="post-title">Selamat datang di blog saya! (i = <?= $i ?>)</h2>
+                            <p class="post-meta"><small>Diposkan pada: 09 Nov 2021 | Oleh: Adnan Zaki</small></p>
+                        </div>
+                        <img src="resources/images/wp-<?= rand(1, 3) ?>.jpg" alt="Gambar <?= $i + 1 ?>" class="post-image">
+                        <div class="post-content">
+                            <?php
+
+                            include_once 'resources/post-excerpt.php';
+                            echo $content;
+
+                            ?>
+                            <a href="#">Read more...</a>
+                        </div>
+                    </div>
+                <?php endfor; ?>
             </div>
-            <div class="sidebar">
-                <div class="judul">
-                    <h2 class="sidebar-title">Recent Post</h2>
-                    <ul class="widget">
-                        <li class="recent-post-list">
-                            <a href="#" class="recent-post">Home</a>
-                        </li>
-                        <li class="recent-post-list">
-                            <a href="#" class="recent-post">Post</a>
-                        </li>
-                        <li class="recent-post-list">
-                            <a href="#" class="recent-post">Kategori</a>
-                        </li>
-                        <li class="recent-post-list">
-                            <a href="#" class="recent-post">Kontak</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <div class="sidebar"></div>
         </div>
-        <div class="footer">
-            <p>&copy; copyright 2021 | CSS layouting .</p>
-        </div>
+        <div class="footer">&copy; copyright 2021 | CSS layouting .</div>
 
     </div>
 </body>
---
+++ css/layout.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layouting | Beginner</title>
+    <link rel="stylesheet" href="layout.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="header">
+            <div class="logo-container">
+                <img src="resources/images/logo-only.png" alt="Logoooooo" class="web-logo">
+            </div>
+            <div class="menu-container">
+                <ul class="nav">
+                    <li class="menu-item atas">
+                        <a href="" class="menu-link ">Home</a>
+                    </li>
+                    <li class="menu-item kanan">
+                        <a href="" class="menu-link ">Post</a>
+                    </li>
+                    <li class="menu-item bawah">
+                        <a href="" class="menu-link ">Category</a>
+                    </li>
+                    <li class="menu-item kiri">
+                        <a href="" class="menu-link ">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="content-wrapper">
+            <div class="content">
+                <div class="content-img">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum explicabo inventore eveniet dicta accusamus, fugiat est minus autem fugit ducimus odio non! Aut saepe culpa amet pariatur enim sed reprehenderit similique vero omnis nisi iste molestiae deleniti, tempore debitis eos impedit, temporibus minus non quasi. Sit adipisci sunt, reprehenderit laudantium excepturi odio cum exercitationem. Non repudiandae dolorum quasi, iusto reprehenderit minima laudantium! Suscipit, deleniti repellat, necessitatibus pariatur commodi cupiditate dicta quis et voluptas odit voluptatibus natus. Dolorem fuga hic mollitia blanditiis ipsum laudantium voluptatem culpa, nulla id ab numquam earum reprehenderit neque quae animi architecto ratione sed. Molestias, labore ipsum sit deserunt possimus laborum quas nemo! Quaerat necessitatibus, facere provident dicta explicabo veniam assumenda eaque est eum expedita et suscipit doloremque vero velit sequi minima rem voluptatibus obcaecati? Tempora officiis ut beatae incidunt ratione nam illo, aperiam sit unde debitis eaque tempore expedita soluta deleniti impedit perspiciatis? Quod quisquam minima accusantium, explicabo nulla eos quasi nobis. Officia quod et ea! Quaerat voluptatem error, quia consectetur voluptatum magni. Voluptates provident impedit odit vitae omnis expedita illo asperiores iure facere maiores! Expedita impedit, optio corrupti quis corporis dicta quisquam odio ratione suscipit aliquid id, veritatis reiciendis fuga, distinctio est fugiat eveniet vero. Quam quis beatae atque saepe doloremque laborum laudantium magnam ducimus obcaecati repellat nesciunt a, sint tenetur id ipsa porro iusto eligendi impedit. Voluptatibus recusandae magni aut possimus, a placeat beatae ea excepturi, dolorum vero ipsam facere illo eveniet maiores incidunt quo numquam distinctio! Obcaecati odit corrupti exercitationem aliquam nobis dolorem corporis sequi tempore. Unde autem enim nisi laboriosam rem omnis illo delectus voluptatibus dolores maiores qui illum ex asperiores, ut quidem voluptatum nam molestiae quasi vitae iusto eaque. Velit nihil, doloribus quia praesentium fuga iure id, esse ab temporibus vitae sapiente error non quam. Laboriosam, nihil quos? Fugiat quo repudiandae consequuntur fuga ipsa vero possimus quidem omnis deserunt tenetur. Repellat commodi fugit neque aut quam ut voluptate, molestiae saepe magni hic autem iste quas itaque repellendus aliquid inventore tempore quasi dolores esse libero sit odit nisi. Quo earum impedit, distinctio, fugit dolorem nobis voluptatibus est ipsam cum placeat veniam voluptates dicta aliquam consectetur quasi ad, quos adipisci nostrum laboriosam praesentium id? Asperiores, illum dolores. Iusto, quod, cupiditate ea sapiente sit necessitatibus sequi, rem sed facere consequatur cum iure velit asperiores amet accusamus ex sunt maxime ullam! Fugiat, sapiente! Non quam quae iusto dignissimos optio voluptas laboriosam recusandae quas qui. Fugiat eaque quidem ipsam atque animi, amet error quod architecto in, quibusdam consequuntur deserunt sunt eos quasi dolorum debitis itaque adipisci sequi eius perspiciatis molestiae consectetur distinctio unde. Ea placeat veritatis similique officia excepturi id itaque? Necessitatibus nobis ipsa dolore molestias similique? Exercitationem odio modi alias obcaecati illum debitis numquam ipsam? Doloremque quis reprehenderit accusamus, quae culpa, ipsa eius quasi iure, ad dolorum itaque consequatur sint et voluptatem numquam vero ut cumque distinctio error optio. Tempora non voluptatem sapiente quibusdam error sint repellendus accusamus, laborum explicabo minima. Quaerat maxime aliquid enim nesciunt nulla alias dolorum, rerum minima quod distinctio est? Similique quibusdam laudantium ipsam numquam voluptas, dolore optio nihil aliquam placeat? Facere provident fugiat ratione numquam perspiciatis magni repellat accusamus, consequuntur iure debitis? Veritatis commodi numquam, consequuntur quis maxime suscipit, laboriosam eos enim nesciunt dolorem, obcaecati voluptatem tempore nihil! Ipsa ad enim veritatis architecto nobis? Modi aut, laborum quisquam doloribus vero perferendis neque inventore, dolorem accusantium ullam odio ratione? Odit ex inventore, vero quo nesciunt accusamus quam eveniet dignissimos iure consequuntur reiciendis rem esse consectetur natus omnis placeat doloribus error nam, quia tempore commodi illo eos, rerum repellendus! Et optio consectetur id aliquid. Ipsa sequi ipsum provident illum aperiam possimus obcaecati accusamus maiores fuga animi, doloribus voluptates corporis quidem ad odio ut tempore commodi modi nobis at quo neque minima. Veritatis alias impedit dignissimos quibusdam dolor, hic cumque, magnam necessitatibus vitae molestias quis aspernatur nihil, aliquam tenetur? Corporis repellat porro rem provident odio libero fugit non. Sed maiores debitis inventore, adipisci alias, molestias repellat tenetur eaque, ipsam nesciunt possimus aperiam? Totam, tenetur officia accusantium, voluptatem quasi soluta veritatis eveniet facere autem dicta numquam odio aliquam quibusdam optio ratione vel assumenda dignissimos, nam cum. Accusamus nesciunt ex molestias eos reprehenderit provident dolor, repellat itaque natus libero doloribus ullam alias, officiis est facere maiores fugit doloremque necessitatibus nam, dicta veniam! Voluptates repudiandae veniam obcaecati sequi, accusamus ut ipsa, dolore nemo, quibusdam neque ipsum assumenda non? Cupiditate ea itaque quibusdam, quae sunt ut nemo blanditiis tenetur dolorem libero facilis est. Blanditiis ex laudantium neque iure, sequi architecto, voluptates nulla alias cumque eligendi exercitationem. Quo beatae inventore consectetur tempore odit dolore cum odio distinctio perferendis recusandae delectus ipsum ullam tenetur reiciendis nulla, cupiditate repellendus! Quas deserunt laborum minima ut commodi temporibus fuga magni aspernatur, vero inventore sed enim neque quod reprehenderit. Harum placeat hic quisquam laboriosam praesentium illum quidem assumenda quia. Culpa, cumque error! Impedit officiis praesentium voluptatum non eaque repudiandae ratione dolorem deleniti cum dolor nemo sit, optio, ea omnis ut atque ipsam qui doloremque tenetur aliquam quibusdam incidunt adipisci culpa? Commodi minima, reprehenderit dolore atque, dignissimos provident repudiandae porro voluptatum excepturi corporis delectus facere sed reiciendis placeat dolorem obcaecati! Enim placeat ab corrupti quis, optio id maxime aperiam minus amet obcaecati in laudantium eligendi, impedit a itaque quia. Eius dolorum quis quisquam sit voluptas quod enim laborum repellat consequuntur perferendis fugiat accusantium voluptates iure et, possimus accusamus eveniet provident temporibus quae itaque. Similique, consequuntur adipisci laudantium nostrum ea aspernatur neque, fugit, facilis natus quo temporibus eaque voluptas minus deleniti et. Quaerat, aperiam eaque placeat facilis velit dicta molestias assumenda. Dolores recusandae aliquam odio earum quasi, consequuntur, magnam repudiandae quis deserunt a vitae debitis, deleniti similique. Ipsa eveniet hic veritatis et, voluptatem mollitia consequatur cum sapiente veniam vitae voluptate harum fugit nobis ab iure. Asperiores nesciunt ullam qui ad cumque velit quisquam quis delectus, sint omnis impedit dolores debitis adipisci. Voluptatibus itaque ratione sint ipsa enim nulla incidunt corrupti illo magnam unde quasi ad ex modi labore amet tempora molestiae a explicabo, blanditiis culpa voluptate sunt illum? Incidunt, reprehenderit. Est eveniet iste asperiores illum.</p>
+                </div>
+            </div>
+            <div class="sidebar">
+
+            </div>
+        </div>
+        <div class="footer">copyright &copy; 2021 | Qais Abdul Aziz.</div>
+
+    </div>
+
+
+</body>
+
+</html>

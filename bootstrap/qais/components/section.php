ae6a1a52409fab68d14896db5aea14b5a79f85d7
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-01-29 23:21:26 +0700

    commit new

---
+++ bootstrap/qais/components/section.php
@@ -0,0 +1,30 @@
+<!-- section 1 -->
+<div class="container-fluid">
+    <div class="row section1 p-5">
+        <div class="col-12 col-md-6">
+            <h2>Pusat Informasi & Koordinasi Grabfood Bekasi</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim necessitatibus pariatur iste eligendi esse error consectetur reiciendis. Architecto magnam qui nisi iusto aut non nulla enim quo accusamus libero.</p>
+            <button class="btn btn-success">Selengkapnya <i class="bi bi-arrow-right"></i></button>
+        </div>
+        <div class="col-12 col-md-6">
+            <img src="../assets/img/gflogo.jpg" class="img-fluid" alt="Grabfood Logo">
+        </div>
+    </div>
+</div>
+<!-- end section1 -->
+
+<!-- section 2 -->
+<div class="container-fluid">
+    <div class="row section2 p-5">
+        <h1 class="col-12 text-center">Section 2 | Judul</h1>
+        <div class="col-12 col-md-6">
+            <img src="../assets/img/gflogo.jpg" class="img-fluid" alt="Grabfood Logo">
+        </div>
+        <div class="col-12 col-md-6">
+            <h2>Pusat Informasi & Koordinasi Grabfood Bekasi</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim necessitatibus pariatur iste eligendi esse error consectetur reiciendis. Architecto magnam qui nisi iusto aut non nulla enim quo accusamus libero.</p>
+            <button class="btn btn-danger">Selengkapnya</button>
+        </div>
+    </div>
+</div>
+<!-- end section2 -->
---
+++ bootstrap/qais/content.php
@@ -0,0 +1,6 @@
+<div class="container"></div>
+<div class="row baris ">
+    <div class="col-12 col-md-6 kolom"></div>
+    <div class="col-12 col-md-6 kolom"></div>
+</div>
+</div>

77e361e5802d12ff90df7279468374499c7e1895
Adnan Zaki <adnan_zaki@hotmail.co.id>
2022-03-06 14:49:27 +0700

    Update example

---
+++ bootstrap/components/cards.php
@@ -0,0 +1,13 @@
+<div class="row">
+  <?php for($i = 0; $i < 4; $i++): ?>
+  <div class="col-12 col-sm-6 col-md-3">
+    <div class="card z-card">
+      <div class="card-header">Perkembangan</div>
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-1">Click aku!</button>
+      </div>
+    </div>
+  </div>
+  <?php endfor; ?>
+</div>
---
+++ bootstrap/components/modal.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="modal-1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore est minima culpa fugiat natus blanditiis animi soluta esse quisquam? Blanditiis nemo, repudiandae modi iure totam repellendus nam eum eligendi accusantium error optio, harum rem molestiae molestias reiciendis quod rerum expedita, aut ipsam soluta quidem. Dolore id vero modi minus tenetur magni dolores nihil culpa, vel incidunt corrupti tempora quos recusandae ipsam, accusamus excepturi veniam! Reiciendis blanditiis tempore, at possimus excepturi asperiores, voluptas qui eos voluptatum quis facere perspiciatis dolore sunt. Mollitia tenetur dolores nihil odit libero voluptate suscipit, molestiae velit perferendis voluptatum quod sunt, veniam deserunt obcaecati illo ipsa autem rerum. Quae quam nihil accusamus, voluptas veniam maxime iure suscipit blanditiis fugit corporis minima eius doloremque provident quisquam doloribus totam quidem deleniti? Natus quibusdam sit accusantium soluta delectus facere laboriosam possimus optio eos necessitatibus architecto sint ut ex tempora iste nihil voluptatum laborum voluptatem, rerum debitis? Cum ratione quasi perspiciatis atque reiciendis eos aliquid voluptates? Excepturi unde quos quasi, vitae fugit laborum voluptas aut architecto eos necessitatibus omnis iste tempore, distinctio non ad aliquid reprehenderit quae animi velit harum. Non consequatur eaque veritatis odit itaque necessitatibus, ab explicabo voluptate. Dolorem mollitia fuga ab quidem nam eveniet fugit vero? Unde, provident!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>

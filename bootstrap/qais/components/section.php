6efe8662fbcf2c7c313d93df53a744dc9d7cd097
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-16 15:00:16 +0700

    commit new

--- bootstrap/qais/components/section.php
+++ bootstrap/qais/components/section.php
@@ -4,7 +4,7 @@
         <div class="col-12 col-md-6">
             <h2>Pusat Informasi & Koordinasi Grabfood Bekasi</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim necessitatibus pariatur iste eligendi esse error consectetur reiciendis. Architecto magnam qui nisi iusto aut non nulla enim quo accusamus libero.</p>
-            <button class="btn btn-success">Selengkapnya <i class="bi bi-arrow-right-circle"></i></button>
+            <button class="btn btn-success">Selengkapnya <i class="bi bi-arrow-right"></i></button>
         </div>
         <div class="col-12 col-md-6">
             <img src="https://cdn.5minvideo.id/images/kumpulan-logo-grabfood-terbaru/grabfood-merchant-logo.jpg" alt="Grabfood Img" class="img-fluid">
@@ -20,22 +20,14 @@
 <!-- section3 -->
 <div class="container-fluid">
     <div class="row section3 pt-5">
-        <div class="col-12 col-md-6 pb-5 ps-5">
+        <div class="col-12 col-md-6 pb-5">
             <img src="https://img.freepik.com/free-vector/young-people-giving-high-five_52683-30029.jpg?w=740" alt="Grabfood Img" class="img-fluid">
         </div>
         <div class="col-12 col-md-6">
             <h6 class="text-success text-capitalize">kenali grabfood</h6>
             <h2>Kenali Informasi Seputar Grabfood </h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ad corporis harum distinctio laboriosam at nam ex dolorum esse unde, reiciendis maiores saepe iusto animi repellat quos soluta eligendi quod?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a dolorum natus sequi error, minima at nesciunt modi neque, dignissimos blanditiis eveniet. Officia exercitationem tenetur natus, totam quae a tempora?</p>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-4 col-sm-12"></div>
-                    <div class="col-12 col-md-4 col-sm-12"></div>
-                    <div class="col-12 col-md-4 col-sm-12 btn-more">
-                        <button class="btn btn-danger">Selengkapnya <i class="bi bi-arrow-right-circle"></i></button>
-                    </div>
-                </div>
-            </div>
+            <button class="btn btn-danger">Selengkapnya <i class="bi bi-arrow-right"></i></button>
         </div>
     </div>
 </div>
@@ -48,7 +40,7 @@
             <h6 class="text-success text-capitalize">penanganan salah kembalian</h6>
             <h2 class="text-capitalize">apa itu tunai, non-tunai, dan promo?</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim necessitatibus pariatur iste eligendi esse error consectetur reiciendis. Architecto magnam qui nisi iusto aut non nulla enim quo accusamus libero.</p>
-            <button class="btn btn-outline-success text-end">Selengkapnya <i class="bi bi-arrow-right-circle"></i></button>
+            <button class="btn btn-outline-success text-end">Selengkapnya <i class="bi bi-arrow-right"></i></button>
         </div>
         <div class="col-12 col-sm-12 col-md-6">
             <img src="https://img.freepik.com/free-vector/man-shows-gesture-great-idea_10045-637.jpg?w=740" class="img-fluid float-end" alt="Freepik Img" style="width: 480px;">
@@ -60,13 +52,13 @@
 <!-- section5 -->
 <div class="container-fluid">
     <div class="row section5 p-5 pl-0">
-        <div class="col col-md-6">
+        <div class="col-12 col-md-6">
             <img src="https://img.freepik.com/free-vector/biotechnology-concept-lab-elements_52683-58738.jpg?w=826" alt="Grabfood Img" class="img-fluid">
         </div>
         <div class="col-12 col-md-6">
             <h2>Pusat Informasi & Koordinasi Grabfood Bekasi</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam enim necessitatibus pariatur iste eligendi esse error consectetur reiciendis. Architecto magnam qui nisi iusto aut non nulla enim quo accusamus libero.</p>
-            <button class="btn btn-success">Selengkapnya <i class="bi bi-arrow-right-circle"></i></button>
+            <button class="btn btn-success">Selengkapnya <i class="bi bi-arrow-right"></i></button>
         </div>
     </div>
 </div>

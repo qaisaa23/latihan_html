222e3a61d7d789d106b2eed88542c9917c28a609
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-16 15:16:00 +0700

    commit new

--- bootstrap/qais/components/sidebar.php
+++ bootstrap/qais/components/sidebar.php
@@ -18,13 +18,13 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
                     </a>
-                    <ul class="dropdown-menu" style="background-color: rgb(93, 197, 93);" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item text-dark" href="#">Action</a></li>
-                        <li><a class="dropdown-item text-dark" href="#">Another action</a></li>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item text-dark" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">

15266a58c403820031c224cb8008903e1d242892
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-17 22:26:11 +0700

    commit new

--- bootstrap/qais/components/cards2.php
+++ bootstrap/qais/components/cards2.php
@@ -5,38 +5,54 @@
             <p class="text-center text-capitalize text-muted pt-2">update terakhir: kamis, 17 maret 2022 22.01</p>
         </div>
     </div>
-    <div class="row">
-        <div class="card-group">
-            <div class="card cards2 m-2" style="border-radius: 8px;">
-                <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
-                <div class="card-body">
-                    <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
-                    <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+    <div class="container-fluid-cards mt-5 pt-3">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12 cd-blue">
+                <div class="container-fluid-cards">
+                    <div class="card cards2 m-2" style="border-radius: 8px;">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
+                        <div class="card-body">
+                            <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
+                            <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card cards2 m-2" style="border-radius: 8px;">
-                <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
-                <div class="card-body">
-                    <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
-                    <p class="card-text h6">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+            <div class="col-lg-3 col-md-6 col-sm-12 cd-yellow">
+                <div class="container-fluid-cards">
+                    <div class="card cards2 m-2" style="border-radius: 8px;">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
+                        <div class="card-body">
+                            <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
+                            <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card cards2 m-2" style="border-radius: 8px;">
-                <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
-                <div class="card-body">
-                    <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
-                    <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+            <div class="col-lg-3 col-md-6 col-sm-12 cd-red">
+                <div class="container-fluid-cards">
+                    <div class="card cards2 m-2" style="border-radius: 8px;">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
+                        <div class="card-body">
+                            <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
+                            <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card cards2 m-2" style="border-radius: 8px;">
-                <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
-                <div class="card-body">
-                    <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
-                    <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+            <div class="col-lg-3 col-md-6 col-sm-12 cd-green">
+                <div class="container-fluid-cards">
+                    <div class="card cards2 m-2" style="border-radius: 8px;">
+                        <img src="https://cdn.pixabay.com/photo/2016/10/16/13/46/mill-1745188_960_720.jpg" class="card-img-top rounded-top" style="height: 140px;" alt="...">
+                        <div class="card-body">
+                            <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
+                            <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text text-capitalize"><small class="text-muted">17 maret 2022</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

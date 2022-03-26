ec320dabb7e6c2c324af497f0367475775b86e22
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-26 22:29:16 +0700

    commit new

--- bootstrap/qais/components/cards2.php
+++ bootstrap/qais/components/cards2.php
@@ -1,11 +1,11 @@
-<div class="container-fluid section6 bg-light pt-5">
+<div class="container-fluid section6 bg-light pt-5 pb-5">
     <div class="row">
         <div class="col-12 pt-5 pb-3">
             <h2 class="text-center text-capitalize">berita terkini</h2>
             <p class="text-center text-capitalize text-muted pt-2">update terakhir: kamis, 17 maret 2022 22.01</p>
         </div>
     </div>
-    <div class="container-fluid-cards mt-5 pt-3">
+    <div class="container-fluid-cards mt-1 pt-1">
         <div class="row">
             <div class="col-lg-3 col-md-6 col-sm-12 cd-blue">
                 <div class="container-fluid-cards">
@@ -56,8 +56,8 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="container-fluid text-center pt-5 pb-5">
+        <div class="row pb-5">
+            <div class="container-fluid text-center pt-4">
                 <button class="btn btn-outline-success cards2-btn">Lihat Berita Selengkapnya <i class="bi bi-arrow-right"></i></button>
             </div>
         </div>

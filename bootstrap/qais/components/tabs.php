dc4a872e6b3269275ef1fc1ef7312355e153dcf3
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-28 13:17:00 +0700

    commit new

--- bootstrap/qais/components/tabs.php
+++ bootstrap/qais/components/tabs.php
@@ -1,19 +1,13 @@
-<div class="container-fluid tabs-container bg-light pb-5 pt-3">
+<div class="container-fluid tabs-container border-info border-top border-bottom bg-light pb-5 pt-3">
     <h3 class="text-bold text-capitalize ps-2 pt-5 pb-5 mb-3">informasi lainnnya</h3>
     <ul class="nav nav-tabs">
         <li class="nav-item">
             <a class="nav-link active text-capitalize" data-bs-toggle="tab" href="#home">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                    <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-                </svg> info praktikal</a>
+                <i class="bi bi-info-circle"></i> info praktikal</a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-capitalize" data-bs-toggle="tab" href="#profile">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
-                    <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z" />
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                </svg> dokumen</a>
+                <i class="bi bi-file-earmark-text"></i> dokumen</a>
         </li>
     </ul>
     <div class="tab-content mb-5">

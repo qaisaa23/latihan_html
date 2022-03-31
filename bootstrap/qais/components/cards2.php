24e1dfac7e8d9e4b56b3ce3ffc920bffc877ebaa
Qais Abdul Aziz <qaizzaa@gmail.com>
2022-03-31 21:14:09 +0700

    add 16x9 image ratio bootstrap 5

--- bootstrap/qais/components/cards2.php
+++ bootstrap/qais/components/cards2.php
@@ -7,10 +7,12 @@
     </div>
     <div class="container-fluid-cards mt-1 pt-1">
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-12 cd-blue">
+            <div class="col-xl-3 col-lg-4 col-md-6 p-4 bg-success cd-blue">
                 <div class="container-fluid-cards">
                     <div class="card cards2 h-100 m-2" style="border-radius: 8px;">
-                        <img src="assets/images/cards2-img1.jpg" class="card-img-top rounded-top" alt="...">
+                        <div class="ratio ratio-16x9">
+                            <img src="assets/images/cards2-img1.jpg" class="card-img-top rounded-top" alt="..." style="object-fit: cover;">
+                        </div>
                         <div class="card-body">
                             <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
                             <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -19,10 +21,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-12 cd-yellow">
+            <div class="col-xl-3 col-lg-4 col-md-6 p-4 bg-warning cd-yellow">
                 <div class="container-fluid-cards">
                     <div class="card cards2 h-100 m-2" style="border-radius: 8px;">
-                        <img src="assets/images/cards2-img2.jpg" class="card-img-top rounded-top" alt="...">
+                        <div class="ratio ratio-16x9">
+                            <img src="assets/images/cards2-img2.jpg" class="card-img-top rounded-top" alt="..." style="object-fit: cover;">
+                        </div>
                         <div class="card-body">
                             <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
                             <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -31,10 +35,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-12 cd-red">
+            <div class="col-xl-3 col-lg-4 col-md-6 p-4 bg-danger cd-red">
                 <div class="container-fluid-cards">
                     <div class="card cards2 h-100 m-2" style="border-radius: 8px;">
-                        <img src="assets/images/cards2-img3.jpg" class="card-img-top rounded-top" alt="...">
+                        <div class="ratio ratio-16x9">
+                            <img src="assets/images/cards2-img3.jpg" class="card-img-top rounded-top" alt="..." style="object-fit: cover;">
+                        </div>
                         <div class="card-body">
                             <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
                             <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
@@ -43,10 +49,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-12 cd-green">
+            <div class="col-xl-3 col-lg-4 col-md-6 p-4 bg-info cd-green">
                 <div class="container-fluid-cards">
                     <div class="card cards2 h-100 m-2" style="border-radius: 8px;">
-                        <img src="assets/images/cards2-img4.jpg" class="card-img-top rounded-top" alt="...">
+                        <div class="ratio ratio-16x9">
+                            <img src="assets/images/cards2-img4.jpg" class="card-img-top rounded-top" alt="..." style="object-fit: cover;">
+                        </div>
                         <div class="card-body">
                             <p class="card-text"><small class="p-border p-1 text-light">Card title</small></p>
                             <p class="card-text h6">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
